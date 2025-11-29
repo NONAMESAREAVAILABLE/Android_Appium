64b3e0093299c32b81b4861dd9dc8b8673a00a72
NONAMESAREAVAILABLE <36884240+NONAMESAREAVAILABLE@users.noreply.github.com>
2025-11-29 20:50:33 +0530

    New code notes

--- ScrolltoElerment/src/test/java/tests/MyScrollUtil.java
+++ ScrolltoElerment/src/test/java/tests/MyScrollUtil.java
@@ -106,6 +106,15 @@ public class MyScrollUtil {
 		driver.perform(java.util.Arrays.asList(swipe));
 
 		*/
+
+		/*
+		int leftX = element.getLocation().getX();
+		int rightX = leftX + element.getSize().getWidth();
+		int middleX = (rightX + leftX) / 2;
+		int upperY = element.getLocation().getY();
+		int lowerY = upperY + element.getSize().getHeight();
+		int middleY = (upperY + lowerY) / 2;
+		*/
 		
 	}
 	
